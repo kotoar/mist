f0d6a6a74533983459a6a5d6648bc1dbb01e96f8
kotoar <kotoar@kotoar.com>
2025-11-20 18:54:45 +0000

    feature: stored game mode

--- lib/client/viewmodel/list.ts
+++ lib/client/viewmodel/list.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { proxy } from "valtio";
 import { caseList } from "@server/case/endpoints";
 import { CasePreview } from "@shared/case-schema";
@@ -5,7 +7,7 @@ import { MistPreview } from "@shared/mist-schema";
 import { mistList } from "@/lib/server/mist/endpoints";
 
 interface ListViewModel {
-  _type: "case" | "mist";
+  _type: "case" | "mist" | null;
   type: "case" | "mist";
   cases: CasePreview[];
   mists: MistPreview[];
@@ -13,12 +15,23 @@ interface ListViewModel {
 }
 
 export const listViewModel = proxy<ListViewModel>({
-  _type: "case",
+  _type: null,
   get type() {
+    if (this._type === null) {
+      const storedType = localStorage.getItem("listType") as "case" | "mist";
+      if (storedType) {
+        this._type = storedType;
+        return this._type;
+      } else {
+        this._type = "case";
+        return this._type;
+      }
+    }
     return this._type;
   },
   set type(value: "case" | "mist") {
     this._type = value;
+    localStorage.setItem("listType", value);
     if (value === "case" && this.cases.length === 0) {
       caseList().then(data => listViewModel.cases = data);
     } else {
@@ -28,7 +41,13 @@ export const listViewModel = proxy<ListViewModel>({
   cases: [],
   mists: [],
   async fetch() {
-    const listData = await caseList();
-    listViewModel.cases = listData;
+    switch (this.type) {
+      case "case":
+        this.cases = await caseList();
+        break;
+      case "mist":
+        this.mists = await mistList();
+        break;
+    }
   }
 });
